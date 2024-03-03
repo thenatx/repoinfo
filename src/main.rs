fcd27366d84fdd5ac278d61724ea9af4d0025c80
NatProgramer <rodrigezsantiago40@gmail.com>
2024-03-03 13:14:46 -0500

    Starting project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+<<<<<<< HEAD
     println!("Hello, world!");
+=======
+    let gh = octocrab::OctocrabBuilder::new().personal_token(String::from("ADODO"));
+
+    octo
+>>>>>>> 47ceadd (Starting project)
 }
