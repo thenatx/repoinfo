765bc837f8c656d6b9b8ba3efa41ad2effe43b20
NatProgramer <rodrigezsantiago40@gmail.com>
2024-03-04 18:28:04 -0500

    Adding commands module to organize the projects

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,13 @@
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-struct ReadmeRes {
-    name: String,
-}
-
 #[tokio::main]
 async fn main() {
-    let user = "NatProgramer";
+    let owner = "NatProgramer";
     let repository = "task-manager-server";
 
-    Ok(())
+
+    commands::get_readme(owner, repository).await;
+    commands::search_repos(repository, 1).await;
+
+    Ok::<(), ()>(()).expect("A unexpected error encountred")
 }
 
-async fn get_readme(owner: &str, repository: &str) {
-    let client = reqwest::Client::new();
-
-    let res = client
-        .get(
-            format!("https://api.github.com/repos/{owner}/{repository}/readme")
-        )
-        .bearer_auth(GITHUB_TOKEN)
-        .header("User-Agent", user)
-        .send().await;
-    
-    match res {
-        Ok(response) => println!("{:#?}", response.json::<ReadmeRes>().await.unwrap().name),
-        Err(error) => {
-            println!("{}", error);
-            std::process::exit(1)
-        }
-    }
-}
+pub mod commands;
