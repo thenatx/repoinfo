aae82e57a7d02f1dea0b4a706763eb406d9b7fc3
NatProgramer <rodrigezsantiago40@gmail.com>
2024-03-10 22:01:46 -0500

    Start using clap

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use clap::Command;
+
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let owner = "NatProgramer";
-    let repository = "repoinfo";
+    let command = Command::new("repoinfo")
+        .about("Get search repositories, get information of repositories and more!");
 
-    commands::get_readme(owner, repository).await;
+    let args = command.args([]);
+
+    println!("{}", args);
+
+    // let owner = "pheralb";
+    // let repository = "svgl";
+
+    // commands::get_readme(owner, repository).await;
     // commands::search::search_repos(repository, 1, 15).await;
+    // commands::repo::repo_information(owner, repository).await;
 
     Ok(())
 }
