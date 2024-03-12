d47ab41df7ac77908cbb7b25d2a0a2711a8c9f11
NatProgramer <rodrigezsantiago40@gmail.com>
2024-03-11 21:24:51 -0500

    Starting using clap to parse commands and make usage easily

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,48 @@
-use clap::Command;
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "repoinfo")]
+#[command(
+    version = "1.0.0",
+    // about = "A CLI for make somethings like search, view and more, lightheight and with some insteresting features"
+)]
+#[command(long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    option: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    // #[command(about = "Search for different things like repositories, issues and Pull requests")]
+    Search(Repository),
+}
+
+#[derive(Debug, Args)]
+struct Repository {
+    #[arg(value_name = "NAME")]
+    name: String,
+    #[arg(required = false, default_missing_value = "0", value_name = "PAGE")]
+    page: u64,
+    #[arg(
+        required = false,
+        default_missing_value = "15",
+        value_name = "NUM_PER_PAGE"
+    )]
+    per_page: u64,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let command = Command::new("repoinfo")
-        .about("Get search repositories, get information of repositories and more!");
+    let args = Cli::parse();
 
-    let args = command.args([]);
-
-    println!("{}", args);
-
-    // let owner = "pheralb";
-    // let repository = "svgl";
+    match args.option {
+        Commands::Search(repository) => {
+            commands::search::search_repos(repository.name.as_str(), 0, 15).await;
+        }
+    }
 
     // commands::get_readme(owner, repository).await;
-    // commands::search::search_repos(repository, 1, 15).await;
     // commands::repo::repo_information(owner, repository).await;
 
     Ok(())
